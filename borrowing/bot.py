2c7c8e764caccbc1f3de693f20b30e78ca4025a7
Andriy <andriykrav@gmail.com>
2023-06-08 15:59:03 +0300

    creating .env file with bot token

--- borrowing/bot.py
+++ borrowing/bot.py
@@ -5,3 +5,7 @@ import telebot
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+config = dotenv_values(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
