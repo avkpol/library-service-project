273df5061b99e15ad0a4481bf4a9cbab2743b187
Andriy <andriykrav@gmail.com>
2023-06-09 22:18:50 +0300

    creating notifications app for creating Telegram bot messaging

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
@@ -9,3 +10,7 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"Borrowing #{self.pk}"
+
+
+
+
--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv, dotenv_values
 
+
 load_dotenv()
 
 config = dotenv_values(".env")
@@ -48,7 +49,9 @@ INSTALLED_APPS = [
     "book",
     "user",
     "borrowing",
+    "payments",
     "notifications",
+
 ]
 
 MIDDLEWARE = [
