6040918d37cde97c29f14ade4b1e0342083999cc
Andriy <andriykrav@gmail.com>
2023-06-14 15:29:56 +0300

    reworked code (user and book fields)

--- borrowing/models.py
+++ borrowing/models.py
@@ -8,8 +8,8 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(null=True, blank=False)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=False)
-    book = models.ForeignKey(Book, default=1, on_delete=models.CASCADE)
-    user = models.ForeignKey(Customer, default=1, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Borrowing #{self.pk}"
